e0d4126913312f84ff9f299dc85c1cdf6c03d788
Valeryia Hrabnitskaya <Valera17041997@mail.ru>
2019-05-24 15:00:24 +0300

    hw1 fix bug and refactor

--- ishop/GoodsList.js
+++ ishop/GoodsList.js
@@ -4,39 +4,52 @@ var GoodsList = React.createClass({
 
   propTypes: {
     tableName: React.PropTypes.string.isRequired,
-    goodsArray:React.PropTypes.arrayOf(
+    goodsArray: React.PropTypes.arrayOf(
       React.PropTypes.shape({
-      code: React.PropTypes.number.isRequired,
-      name: React.PropTypes.string.isRequired,
-      cost: React.PropTypes.number.isRequired,
-      goodUrl: React.PropTypes.string.isRequired,
-      pictureName: React.PropTypes.string.isRequired,
-      numbers: React.PropTypes.number.isRequired
+        code: React.PropTypes.number.isRequired,
+        name: React.PropTypes.string.isRequired,
+        cost: React.PropTypes.number.isRequired,
+        goodUrl: React.PropTypes.string.isRequired,
+        pictureName: React.PropTypes.string.isRequired,
+        numbers: React.PropTypes.number.isRequired
+      })
+    ),
+    tableHeaders: React.PropTypes.arrayOf(
+      React.PropTypes.shape({
+        headerCode: React.PropTypes.number.isRequired,
+        headerName: React.PropTypes.string.isRequired,
+        headerCost: React.PropTypes.string.isRequired,
+        headerPicture: React.PropTypes.string.isRequired,
+        headerNumbers: React.PropTypes.string.isRequired
       })
     )
   },
 
-  render: function() {
+  render: function () {
 
-    var goods=this.props.goodsArray.map( good =>
-      React.DOM.tr({key:good.code},
-        React.DOM.td({className:'GoodItem'},good.name),
-        React.DOM.td({className:'GoodItem'},good.cost),
-        React.DOM.td({className:'GoodItem'},
-        React.DOM.img({className:'GoodImage', src:good.goodUrl, alt: good.pictureName}) 
+    var goods = this.props.goodsArray.map(good =>
+      React.DOM.tr({ key: good.code },
+        React.DOM.td({ className: 'GoodItem' }, good.name),
+        React.DOM.td({ className: 'GoodItem' }, good.cost),
+        React.DOM.td({ className: 'GoodItem' },
+          React.DOM.img({ className: 'GoodImage', src: good.goodUrl, alt: good.pictureName })
         ),
-        React.DOM.td({className:'GoodItem'},good.numbers),
+        React.DOM.td({ className: 'GoodItem' }, good.numbers),
       )
     );
-    return React.DOM.table({className:'GoodsList'}, 
-      React.DOM.caption( {className:'ListCaption'}, this.props.tableName ),
-      React.DOM.tr( {},
-        React.DOM.th( {className:'TableHeader'}, this.props.tableHeaders.headerName),
-        React.DOM.th( {className:'TableHeader'}, this.props.tableHeaders.headerCost),
-        React.DOM.th( {className:'TableHeader'}, this.props.tableHeaders.headerPicture),
-        React.DOM.th( {className:'TableHeader'}, this.props.tableHeaders.headerNumbers),
-      ),
-      React.DOM.tbody({}, goods),
+    var headers = this.props.tableHeaders.map(header =>
+      React.DOM.tr({ key: header.headerCode },
+        React.DOM.th({ className: 'TableHeader' }, header.headerName),
+        React.DOM.th({ className: 'TableHeader' }, header.headerCost),
+        React.DOM.th({ className: 'TableHeader' }, header.headerPicture),
+        React.DOM.th({ className: 'TableHeader' }, header.headerNumbers),
+      )
+    );
+
+    return React.DOM.table({ className: 'GoodsList' },
+      React.DOM.caption({ className: 'ListCaption' }, this.props.tableName),
+      React.DOM.thead({}, headers),
+      React.DOM.tbody({}, goods)
     );
   },
 
