df35f11bf381e411ac5b7780aa974f3f1cb8f998
Valeryia Hrabnitskaya <Valera17041997@mail.ru>
2019-06-03 20:31:36 +0300

    hw2.2 refactor GoodItem

--- ishop2/GoodItem.js
+++ ishop2/GoodItem.js
@@ -19,19 +19,21 @@ var GoodItem = React.createClass({
   },
 
   deleteRow: function (e) {
+    e.stopPropagation();
     this.props.deleteSelecredRow(this.props.code)
   },
 
   render: function () {
     return React.DOM.tr({
-      className: this.props.isSelected ? 'GoodRowSelected' : 'GoodRow'
+      className: this.props.isSelected ? 'GoodRowSelected' : 'GoodRow',
+      onClick: this.rowSelected
     },
-      React.DOM.td({ className: 'GoodItem', onClick: this.rowSelected }, this.props.name),
-      React.DOM.td({ className: 'GoodItem', onClick: this.rowSelected }, this.props.cost),
-      React.DOM.td({ className: 'GoodItem', onClick: this.rowSelected },
-        React.DOM.img({ className: 'GoodImage', src: this.props.goodUrl, alt: this.props.pictureName, onClick: this.rowSelected })
+      React.DOM.td({ className: 'GoodItem' }, this.props.name),
+      React.DOM.td({ className: 'GoodItem' }, this.props.cost),
+      React.DOM.td({ className: 'GoodItem' },
+        React.DOM.img({ className: 'GoodImage', src: this.props.goodUrl, alt: this.props.pictureName })
       ),
-      React.DOM.td({ className: 'GoodItem', onClick: this.rowSelected }, this.props.numbers),
+      React.DOM.td({ className: 'GoodItem' }, this.props.numbers),
       React.DOM.td({ className: 'GoodItem' },
         React.DOM.input({ type: 'button', className: 'DeleteInput', value: 'Delete', onClick: this.deleteRow })
       )
