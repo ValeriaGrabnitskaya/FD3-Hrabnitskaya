f105c973087aa47fba10a5f64397deaedb5ff5f2
Valeryia Hrabnitskaya <VHrabnitskaya@iba.by>
2019-06-20 19:59:42 +0300

    hw4.2 add basic component

---
+++ RainbowFrame/App.js
@@ -0,0 +1,14 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import RainbowFrame from './RainbowFrame';
+
+let colors = ['red', 'orange', 'yellow', 'green', '#00BFFF', 'blue', 'purple'];
+
+ReactDOM.render(
+  <RainbowFrame colors={colors}>
+    Hello!
+  </RainbowFrame>, document.getElementById('container')
+)
---
+++ RainbowFrame/RainbowFrame.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class RainbowFrame extends React.Component {
+
+    static propTypes = {
+        colors: PropTypes.array.isRequired
+    };
+
+    render() {
+    }
+}
+
+export default RainbowFrame;
