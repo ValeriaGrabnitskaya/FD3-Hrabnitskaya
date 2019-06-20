31e22838009346a5f019b725db1e901c9597ec87
Valeryia Hrabnitskaya <VHrabnitskaya@iba.by>
2019-06-20 13:57:52 +0300

    hw4.1 add correct array

--- br2jsx/BR2JSX.js
+++ br2jsx/BR2JSX.js
@@ -2,14 +2,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './BR2JSX.css';
+
 const BR2JSX = (props) => {
     const textArray = props.text.match(/[а-я]+/g);
-
-    const mapTextArray = ["первый", <br />, "второй"];
-
+    const mas = [];
+    textArray.forEach((element, i) => {
+        if (i + 1 === textArray.length) {
+            mas.push(element)
+        } else {
+            mas.push(element, <br key={i + 1} />)
+        }
+    });
     return (
-        <div>
-            {mapTextArray}
+        <div className='Block'>
+            {mas}
         </div>
     )
 }
