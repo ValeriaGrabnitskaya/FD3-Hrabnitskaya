b4f800e804165e5fc456a777c4b1a06be257336c
Valeryia Hrabnitskaya <VHrabnitskaya@iba.by>
2019-06-21 11:28:38 +0300

    hw4.2 add function getDiv

--- RainbowFrame/RainbowFrame.js
+++ RainbowFrame/RainbowFrame.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './RainbowFrame.css';
+
 class RainbowFrame extends React.Component {
 
     static propTypes = {
         colors: PropTypes.array.isRequired
     };
 
+    getDiv = (color, i, children) => {
+        return <div key={i} style={{ border: "solid 3px " + color, padding: "10px", textAlign:"center" }}>{children}</div>
+    }
+
     render() {
+        let divBlock = this.props.children;
+        this.props.colors.forEach((color, i) => {
+            divBlock = this.getDiv(color, i, divBlock)
+        })
+        return (
+            <div className="DivBlock">
+                {divBlock}
+            </div>
+        )
     }
 }
 
